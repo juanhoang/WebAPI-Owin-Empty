df817e1acbdd4308cd56086703c708e7494131bd
Huan HOANG <huanhvhd@gmail.com>
2015-06-19 18:38:50 +0700

    Create template projects

---
+++ WebAPI-Owin-Template-Minimal/WebApiOwinTemplateSelfhost/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+
+namespace WebApiOwinTemplateSelfhost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string baseAddress = "http://localhost:9000/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(baseAddress))
+            {
+                Console.WriteLine("Application started");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ WebAPI-Owin-Template-Minimal/WebApiOwinTemplateSelfhost/Registers/WebApiRegister.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+
+namespace WebApiOwinTemplateSelfhost.Registers
+{
+    public class WebApiRegister
+    {
+        public static HttpConfiguration BuildHttpConfiguration()
+        {
+            var config = new HttpConfiguration();
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/id",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            return config;
+        }
+    }
+}
