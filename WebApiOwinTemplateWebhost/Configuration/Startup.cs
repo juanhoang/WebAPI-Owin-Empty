0af6bfcd6925331c1710d5f5df5a58cc473bde4c
Huan HOANG <huanhvhd@gmail.com>
2015-06-22 18:14:08 +0700

    V1

---
+++ WebApiOwinTemplateWebhost/Configuration/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using WebApiOwinTemplateWebhost.Configuration;
+
+[assembly:OwinStartup(typeof(Startup))]
+
+namespace WebApiOwinTemplateWebhost.Configuration
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            appBuilder.UseWebApi(WebApiConfig.Register());
+        }
+    }
+}
---
+++ WebApiOwinTemplateWebhost/Configuration/WebApiConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+
+namespace WebApiOwinTemplateWebhost.Configuration
+{
+    public static class WebApiConfig
+    {
+        public static HttpConfiguration Register()
+        {
+            var config = new HttpConfiguration();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            return config;
+        }
+    }
+}
