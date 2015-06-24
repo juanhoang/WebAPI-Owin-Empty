5924d949512e65711753cda276499e603fd0f538
juanonsoftware <huanhvhd@gmail.com>
2015-06-24 18:28:20 +0700

    Add a sample

---
+++ MyWebApiOwinCors/Configuration/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using MyWebApiOwinCors.Configuration;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace MyWebApiOwinCors.Configuration
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            appBuilder.UseWebApi(WebApiConfig.Register());
+        }
+    }
+}
