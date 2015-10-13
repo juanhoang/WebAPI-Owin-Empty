321a4c93cf399cfa025a68386136c4c6c482dbeb
juanonsoftware <juanonsoftware@github.com>
2015-10-13 18:21:03 +0700

    Refactor Startup.cs

---
+++ WebApiOwinTemplateWebhost/Configuration/IocConfig.cs
@@ -0,0 +1,40 @@
+﻿using Rabbit.IOC;
+using Rabbit.SimpleInjectorDemo.IocModules;
+using SimpleInjector;
+using SimpleInjector.Integration.WebApi;
+using SimpleInjector.Packaging;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebApiOwinTemplateWebhost.Configuration
+{
+    public static class IocConfig
+    {
+        public static void RegisterDependencyResolver(this HttpConfiguration configuration)
+        {
+            var container = BuildContainer();
+            configuration.DependencyResolver = new SimpleInjectorWebApiDependencyResolver(container);
+        }
+
+        private static Container BuildContainer()
+        {
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new WebApiRequestLifestyle();
+
+            // TODO: Update all assemblies which contain IoC modules here
+            var possibleAssemblies = new[]
+            {
+                typeof (WebApiConfig).Assembly,
+                typeof (DemoServicesModule).Assembly
+            };
+
+            ModuleHelper.GetModuleTypes(possibleAssemblies)
+                .CreateModules()
+                .Cast<IPackage>()
+                .ToList()
+                .ForEach(x => x.RegisterServices(container));
+
+            return container;
+        }
+    }
+}
---
+++ WebApiOwinTemplateWebhost/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using Owin;
+using WebApiOwinTemplateWebhost;
+using WebApiOwinTemplateWebhost.Configuration;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace WebApiOwinTemplateWebhost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var configuration = WebApiConfig.Register();
+
+            //  Register an IOC
+            configuration.RegisterDependencyResolver();
+
+            appBuilder.UseWebApi(configuration);
+        }
+    }
+}
