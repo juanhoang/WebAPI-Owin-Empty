7dfc947ba52c7ef9e4f7e5d493936e0ab698d91b
juanonsoftware <juanonsoftware@github.com>
2015-10-14 22:17:57 +0700

    Add custom auth providers

--- WebApiExternalAuth/Controllers/AccountController.cs
+++ WebApiExternalAuth/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Http;
+using WebApiExternalAuth.Models;
 
 namespace WebApiExternalAuth.Controllers
 {
---
+++ WebApiExternalAuth/Models/FacebookCustomAuthenticationProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security.Facebook;
+
+namespace WebApiExternalAuth.Models
+{
+    public class FacebookCustomAuthenticationProvider : FacebookAuthenticationProvider
+    {
+        public override Task Authenticated(FacebookAuthenticatedContext context)
+        {
+            context.Identity.AddClaim(new Claim(Claims.ExternalAccessToken, context.AccessToken));
+            context.Identity.AddClaim(new Claim(Claims.ExternalEmail, context.Email));
+            context.Identity.AddClaim(new Claim(Claims.ExternalUserName, context.UserName));
+
+            return base.Authenticated(context);
+        }
+    }
+}
